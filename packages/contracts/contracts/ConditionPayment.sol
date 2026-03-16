3f6963f1a4010a6db8886f6f159ca13811ac82c5
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-03-16 13:32:56 +0200

    conditional pay in=mports

--- packages/contracts/contracts/ConditionPayment.sol
+++ packages/contracts/contracts/ConditionPayment.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../interfaces/IERC20.sol";
-import "../interfaces/IMarket.sol";
+import "./IERC20.sol";
+import "./iMarket.sol";
 
 
  
--- packages/contracts/contracts/SubscriptionManager.sol
+++ packages/contracts/contracts/SubscriptionManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "../interfaces/IERC20.sol";
+import "./IERC20.sol";
 
 
 
